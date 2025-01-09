603363faabeb510278d6a7e2307fd55215abf5bd
Safayet Ullah <sojibmirzabd@gmail.com>
2025-01-09 22:03:57 +0600

    C

---
+++ ASCII to Character.cpp
@@ -0,0 +1,11 @@
+//this is a program for convert the ASCII value into character value//
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+    int n;
+    printf("Enter any number:  ");
+    scanf("%d",&n);
+    printf("The Character Value is = %c",n);
+    getch();
+}
